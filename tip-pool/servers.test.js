d351865d3aad21104d0c658d681b55a882cb6b72
Floyd Ferrer <floydferrer@hotmail.com>
2023-10-14 10:34:26 -0700

    completed servers.test

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -11,7 +11,20 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should update server table with server name and earnings', function () {
+    submitServerInfo();
+    updateServerTable();
+    let server = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(server.length).toEqual(2);
+    expect(server[0].innerText).toEqual('Alice');
+    expect(server[1].innerText).toEqual('$0.00');
+  });
+
   afterEach(function() {
     // teardown logic
+    serverId = 0;
+    serverTbody.innerHTML = ''
+    allServers = {};
   });
 });
